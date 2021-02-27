fa409af4b62cf3a0f43cca5d817fe659e39e79d4
Gian Claudio Scarafini <64615850+GianClaudioScarafini@users.noreply.github.com>
2021-02-27 16:24:11 +0000

    DosomeFile

--- GetListOfDoc.py
+++ GetListOfDoc.py
@@ -1,5 +1,23 @@
 import os
+from pathlib import Path
+
 
 a = os.path.dirname(os.path.abspath(__file__))
 
-print("thisimy path: \n" + str(a))
+print(Path.cwd())
+
+
+
+FileNames = ["file-number-1" +str(i) for i in range (10)]
+
+
+## this fucntion create a list of .txt file
+
+
+def Fromlistcretefile(mylist):
+    for i in mylist:
+        file = open(str(i)+".pdf", "w") 
+        file.write("the file name is: " + str(i)) 
+        file.close()        
+
+#Fromlistcretefile(FileNames)
